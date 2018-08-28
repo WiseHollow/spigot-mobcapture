2e1516138a78aca4f06ab18ad59765e4eaaa0cd6
WiseHollow <ieatlotz@gmail.com>
2018-08-28 15:49:26 -0600

    Mobs will now fit into monster category, or they default into peaceful.

--- src/net/johnbrooks/mh/managers/PermissionManager.java
+++ src/net/johnbrooks/mh/managers/PermissionManager.java
@@ -10,11 +10,9 @@ public class PermissionManager {
     public final String CatchHostile = "MobCapture.Catch.Hostile";
 
     public boolean hasPermissionToCapture(Player player, LivingEntity livingEntity) {
-        if (livingEntity instanceof Animals && player.hasPermission(CatchPeaceful)) {
+        if (livingEntity instanceof Monster && player.hasPermission(CatchHostile)) {
             return true;
-        } else if (livingEntity instanceof WaterMob && player.hasPermission(CatchPeaceful)) {
-            return true;
-        } else if (livingEntity instanceof Monster && player.hasPermission(CatchHostile)) {
+        } else if (livingEntity instanceof Mob && player.hasPermission(CatchPeaceful)) {
             return true;
         } else {
             return player.hasPermission(CatchPrefix + livingEntity.getType().name());
